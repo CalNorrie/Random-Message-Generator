a70031c4f46ff5b27a6605d3bd07c5b61d8c31db
CalNorrie <calnorrie@gmail.com>
2023-06-19 19:47:24 +0100

    Add functions to return random adjectives and verbs to main.js

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const randomNouns = () => {
 }
 
 console.log(randomNouns());
+
+const randomAdjective = () => {
+    const adjectiveList = ['good', 'bad', 'questionable', 'immaculate', 'horrific', 'confusing', 'memorable', 'cataclysmic', 'boring', 'marvellous']
+    return adjectiveList[Math.floor(Math.random() * 10)]
+}
+
+console.log(randomNouns());
+
+const randomVerb = () => {
+    const verbList = ['can dunk', 'run', 'wait', 'push on', 'fight', 'remember', 'forget', 'strive for greatness', 'skip leg day', 'recognise the trebuchet as the superior seige weapon of the middle ages']
+    return verbList[Math.floor(Math.random() * 10)]
+}
+
+console.log(randomNouns());
